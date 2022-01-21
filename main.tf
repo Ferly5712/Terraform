da417cea2c27676fb8a09bb3406f7f9b89ce9be5
Ferly5712 <dedehbloger@gmail.com>
2022-01-21 22:29:11 +0800

    commit

--- main.tf
+++ main.tf
@@ -28,3 +28,28 @@ variable "password" {
   type = "string"
   default = "Test1234!"
 }
+provider "alicloud" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region = "${var.region}"
+}
+
+data "alicloud_images" "search" {
+  name_regex = "^ubuntu_16.*_64"
+}
+data "alicloud_instance_types" "default" {
+  instance_type_family = "ecs.xn4"
+  cpu_core_count = 1
+  memory_size = 1
+}
+resource "alicloud_instance" "web" {
+  instance_name = "${var.name}"
+  image_id = "${data.alicloud_images.search.images.0.image_id}"
+  instance_type = "${data.alicloud_instance_types.default.instance_types.0.id}"
+
+  vswitch_id = "${var.vswitch}"
+  security_groups = "${var.sgroups}"
+  internet_max_bandwidth_out = 100
+  allocate_public_ip = true
+
+  password = "${var.password}"
